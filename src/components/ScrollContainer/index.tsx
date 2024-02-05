23b151d72ce43fa689a123eea0de58b8fd9fc369
MaikonWeber <maikonweber@gmail.com>
2024-02-05 17:35:34 -0300

    Add ScrollCointainer

--- src/components/ScrollContainer/index.tsx
+++ src/components/ScrollContainer/index.tsx
@@ -1,15 +1,30 @@
 // ScrollContainer.tsx
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Pokemon {
+  name: string;
+}
 
 const ScrollContainer: React.FC = () => {
-  const [items, setItems] = useState<number[]>([]);
+  const [items, setItems] = useState<Pokemon[]>([]);
 
   // Simulated API call to fetch more items
-  const fetchMoreItems = () => {
-    const newItems = Array.from({ length: 10 }, (_, index) => index + items.length + 1);
-    setItems((prevItems) => [...prevItems, ...newItems]);
-  };
+  const fetchMoreItems = async () => {
+    try {
+      const response = await axios.get('https://pokeapi.co/api/v2/pokemon', {
+        params: {
+          limit: 10,
+          offset: items.length,
+        },
+      });
 
+      const newPokemonList = response.data.results;
+      setItems((prevItems) => [...prevItems, ...newPokemonList]);
+    } catch (error) {
+      console.error('Error fetching more items:', error);
+    }
+  };
   // Intersection Observer callback to fetch more items when user reaches the bottom
   const handleIntersection = (entries: IntersectionObserverEntry[]) => {
     if (entries[0].isIntersecting) {
@@ -18,6 +33,7 @@ const ScrollContainer: React.FC = () => {
   };
 
   useEffect(() => {
+    fetchMoreItems();
     // Create an Intersection Observer
     const observer = new IntersectionObserver(handleIntersection, {
       root: null,
@@ -38,10 +54,10 @@ const ScrollContainer: React.FC = () => {
   }, [items]);
 
   return (
-    <div className="flex items-center justify-center bg-red-100 max-h-40 overflow-y-auto border-t mt-4 p-4">
-      {items.map((item) => (
-        <div key={item} className="border-b py-2" id={item === items[items.length - 1] ? 'last-item' : undefined}>
-          Item {item}
+    <div className="bg-red-100 max-h-full overflow-y-auto border-t mt-4 p-4">
+      {items.map((pokemon, index) => (
+        <div key={pokemon.name} className="border-b py-2" id={index === items.length - 1 ? 'last-item' : undefined}>
+          {pokemon.name}
         </div>
       ))}
     </div>
