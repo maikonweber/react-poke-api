ba06992a3f31d567705f55282a26d3da540fbde0
MaikonWeber <maikonweber@gmail.com>
2024-02-05 17:21:38 -0300

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header'
 import InputComponent from './components/InputComponent';
 import ScrollContainer from './components/ScrollContainer'
+import Footer from './components/Footer';
 
 const  App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const  App: React.FC = () => {
     <Header />
     <InputComponent />
     <ScrollContainer />
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+// Footer.tsx
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 mt-4">
+      <div className="container mx-auto">
+        <p className="text-center">
+          &copy; 2022 Your Company | All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
